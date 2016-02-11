ee487734b60aa07545f7d63d2a345ec6a3b586dd
Jnillo <jnillo9@gmail.com>
2016-02-11 20:11:20 +0000

    add gibbon and service for mailchimp

---
+++ app/controllers/subscriptions_controller.rb
@@ -0,0 +1,30 @@
+class SubscriptionsController < ApplicationController
+  include Services::Mailchimp
+
+  def create
+    if subscribe_user(subscription_list, subscriber_params)
+      flash[:notice] = 'Suscrito!'
+    else
+      flash[:error] = 'No suscrito'
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def subscriber_params
+  	{
+  	  body: {
+  	  	email_address: params['subscription']['email'],
+  	  	status: 'subscribed',
+  	  	merge_fields: {
+  	  	  'FNAME': params['subscription']['name']
+  	  	}
+  	  }
+  	}
+  end
+
+  def subscription_list
+  end
+
+end
