875cf8f886556b1bf322f0b9927edf7aa377642b
Jnillo <juan.vazquez@nextail.co>
2017-09-02 12:07:53 +0200

    post player for podcasts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,5 @@ Rails.application.config.assets.version = '1.1'
 # Precompile additional assets.
 Rails.application.config.assets.compile = true
 Rails.application.config.assets.precompile += %w( application.js application.css clear_form.scss clean_form.js.coffee markdown_converter.js.coffee medium.js medium.min.css medium-editor-insert-plugin.min.js sortable.min.js sortable.min.js jquery.ui.widget.min.js jquery.iframe_transport.js  jquery.fileupload.min.js commons.scss fonts.scss)
-Rails.application.config.assets.precompile += %w( patricia_carmona_colors.scss patricia_carmona.scss modal_link_observer.js.coffee font-families.css mobile.scss patricia_carmona_mobile.scss jquery.inifinitescroll.js infinite_scroll.js audio.min.js audio.css)
+Rails.application.config.assets.precompile += %w( patricia_carmona_colors.scss patricia_carmona.scss modal_link_observer.js.coffee font-families.css mobile.scss patricia_carmona_mobile.scss jquery.inifinitescroll.js infinite_scroll.js audio.min.js podcasts.css podcasts_mobile.css)
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
